58285681f99a4897111f0be8a921f8e94be3b377
sambeth <sambethslim@gmail.com>
2020-11-27 07:47:49 +0000

    added test to check for valueerror exception

--- src/tests/endpoint_tests.py
+++ src/tests/endpoint_tests.py
@@ -1,4 +1,4 @@
-import requests
+import pytest
 from configparser import ConfigParser
 from get_data_from_api import get_movies_in_theatres, get_movies_on_tv
 
@@ -17,3 +17,13 @@ def test_movies_in_theatres_by_zip_code_and_by_start_date_equals_200():
 def test_movies_on_tv_by_line_id_and_by_start_datetime_equals_200():
     response = get_movies_on_tv("2020-11-26T12:00Z", "USA-TX42500-X", api_key)
     assert response.status_code == 200
+
+
+def test_movies_in_theatres_by_zip_code_and_by_start_date_raises_value_error():
+    with pytest.raises(ValueError):
+        get_movies_in_theatres("", "", api_key)
+
+
+def test_movies_on_tv_by_line_id_and_by_start_datetime_raises_value_error():
+    with pytest.raises(ValueError):
+        get_movies_on_tv("", "", api_key)
